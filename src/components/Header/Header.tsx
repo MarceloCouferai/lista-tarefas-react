d7f59764b71558cf4c2412acfdeff9dab30a68e8
Marcelo <mcouferai@gmail.com>
2024-07-22 15:07:39 -0300

    Começando Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import styles from './Header.module.scss'
+import { StatsCard } from '../StatsCard/StatsCard'
+
+export const Header: React.FC = () => {
+    return(
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <div>
+                    <h1>MyTodo</h1>
+                    <span>Bem-vindo, Marcelo!</span>
+                </div>
+                <div>
+                    <StatsCard title="Total de tarefas" value={5}/>
+                    <StatsCard title="Tarefas Pendentes" value={4}/>
+                    <StatsCard title="Tarefas Concluidas" value={1}/>
+                </div>
+            </div>
+        </header>
+    )
+}
